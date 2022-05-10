310f4c79ea83760c038cf417884e481bacd9b8ab
Dmitry Vorobev <vorobyev.d.o@gmail.com>
2022-05-11 01:56:50 +0300

    Add Example_004

---
+++ Example_004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Данная программа покажет все четные числа от 1 до введенного пользователем числа");
+Console.Write("Введите целое число больше еденицы: ");
+int num = int.Parse(Console.ReadLine());
+int count = 2;
+if (num>1)
+{
+    while ( num >= count )
+    {
+    Console.WriteLine(count);
+    count = count + 2;
+    }
+}
+else Console.Write("Неверное число");
+Console.ReadKey();
