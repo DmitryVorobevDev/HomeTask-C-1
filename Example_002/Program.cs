bf7ee5103d638b7708d9f864978775867a9aa7a2
Dmitry Vorobev <vorobyev.d.o@gmail.com>
2022-05-11 01:20:27 +0300

    Add Example_002

---
+++ Example_002/Program.cs
@@ -0,0 +1,21 @@
+﻿// : Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Данная программа сравнивает три целых числа");
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+int maxnum = num1;
+if (num1>=num2) 
+{
+    maxnum = num1;
+    if (num3>=num1) maxnum = num3;
+}
+else if(num2>=num1)
+{
+    maxnum = num2;
+    if (num3>=num2) maxnum = num3;
+}
+Console.Write(maxnum);
+Console.ReadKey();
